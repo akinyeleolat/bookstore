5a08b4e2f95801b5c939fdce7175ceb611bc7d61
tosinakinyele <oluwatosin@esettlementgroup.com>
2021-11-29 09:35:55 +0100

    add response model

---
+++ src/main/java/com/application/bookstore/model/ResponseModel.java
@@ -0,0 +1,46 @@
+package com.application.bookstore.model;
+
+package com.esl.walletservice.models;
+
+public class ResponseModel {
+
+    private String status;
+    private String message;
+    private Object data;
+
+    public ResponseModel(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ResponseModel(String status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+}
